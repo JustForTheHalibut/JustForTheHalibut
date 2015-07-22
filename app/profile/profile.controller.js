9402d0ee288139c9cb919244fa3d0dffb5b5a317
Trevor Glass <glasser1988@gmail.com>
2015-07-22 09:06:02 -0400

    not to lose

--- app/profile/profile.controller.js
+++ app/profile/profile.controller.js
@@ -2,7 +2,12 @@ angular
   .module('profile')
   .controller('TabsController', function ($scope) {
 
+  })
+
+  .controller('ProgressController', function ($scope) {
+  $scope.max = 200;
 })
+
   .controller('ProfileController', function($scope, $auth, $alert, Account) {
 
     /**
